3392c43490ee89b0f9039c080dba6ca3df64ce85
12944qwerty <qwerty12944qwerty@gmail.com>
2019-04-18 11:28:44 -0500

    Create scratch-cmt-id.js
    not working but decided to put on github

---
+++ scratch-cmt-id.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name         Scratch comment ID
+// @namespace    https://github.com/12944qwerty
+// @version      0.1
+// @description  Get the comment id of a comment in scratch
+// @author       12944qwerty
+// @match        https://scratch.mit.edu/users/*
+// @match        https://scratch.mit.edu/studios/*/comments
+// @match        https://scratch.mit.edu/projects/*
+// ==/UserScript==
+
+// NOTES
+// https://scratch.mit.edu/projects/146100082/
+
+
+var comments = document.getElementsByClassName("comment ");
+for(let cmt of comments) {
+    var content = comments.getElementByClassName("content");
+    content.innerHTML += '<br/><br/>'+cmt.id;
+}
