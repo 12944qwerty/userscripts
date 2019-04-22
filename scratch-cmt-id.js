9b0780a9f94bcadecc6f19667ac56de5cfe31f74
12944qwerty <qwerty12944qwerty@gmail.com>
2019-04-22 09:25:46 -0500

    workss

--- scratch-cmt-id.js
+++ scratch-cmt-id.js
@@ -1,21 +1,21 @@
 // ==UserScript==
-// @name         scratch-comment ID
+// @name         scratch-comment-id
 // @namespace    https://github.com/12944qwerty
-// @version      0.1
+// @version      0.2
 // @description  Get the comment id of a comment in scratch
-// @author       12944qwerty
+// @author       12944qwerty and kenny2github
 // @updateURL    https://github.com/12944qwerty/userscripts/raw/master/scratch-cmt-id.js
 // @match        https://scratch.mit.edu/users/*
 // @match        https://scratch.mit.edu/studios/*/comments
 // @match        https://scratch.mit.edu/projects/*
+// @require      http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js
+// @require      https://gist.github.com/raw/2625891/waitForKeyElements.js
 // ==/UserScript==
 
-// NOTES
-// https://scratch.mit.edu/projects/146100082/
-
-
-var comments = document.getElementsByClassName("comment ");
-for(let cmt of comments) {
-    var content = comments.getElementByClassName("content");
-    content.innerHTML += '<br/><br/>'+cmt.id;
-}
+waitForKeyElements('.comment', function(comment){
+    comment = comment.get(0);
+    var content = comment.querySelector('.content');
+    content.innerHTML += '<hr/>';
+    content.innerHTML += '(<a>Comment ID: ' + comment.dataset.commentId + '</a>&nbsp;';
+    content.innerHTML += '|&nbsp;<a href="' + window.location.origin + window.location.pathname + '#comments-' + comment.dataset.commentId + '">Link</a>)';
+});
